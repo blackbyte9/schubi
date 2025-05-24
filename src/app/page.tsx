13578436a54a57bca901e9b1726349f5f2de6820
Black Byte <blackbyte9@gmx.net>
2025-05-24 11:03:26 +0200

    Add Authentication including fix for the redirect error in signOut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import { auth } from "@/auth";
+import AuthButton from "@/component/auth/AuthButton.server";
+
+export default async function Page() {
+  const session = await auth();
+  return (
+    <main>
+      <h1>Hello, Next.js!</h1>
+      <pre>{JSON.stringify(session, null, 2)}  </pre>
+      <AuthButton />
+    </main>
+  );
 }
---
+++ src/lib/auth/helpers.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { options } from './../../../node_modules/tsconfig-paths/src/options';
+import { signIn as nSignIn, signOut as nSignOut } from "@/auth";
+
+export async function signIn() {
+    await nSignIn();
+}
+
+export async function signOut(options?: { redirect?: boolean }) {
+    await nSignOut(options);
+}
